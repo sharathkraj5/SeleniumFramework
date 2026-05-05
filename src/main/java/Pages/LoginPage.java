c603a054b9762e6377ff31aea6f2a94a139c1e54
sharathkraj5 <sharathkraj5@gmail.com>
2026-05-05 14:44:28 +0530

    Paga factory implementation

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -2,36 +2,56 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 	
 	private WebDriver driver;
 	
 	
-	private By usernameText = By.id("username");
-	private By passwordText = By.id("password");
-	private By loginButton    = By.name("login");
+	// Page Factory method
+	@FindBy(id="username")
+	WebElement usernameText;
+	
+	@FindBy(id="password")
+	WebElement passwordText;
+	
+	@FindBy(name="login")
+	WebElement loginButton;
+	
+	
+	
+	
+//	private By usernameText = By.id("username");
+//	private By passwordText = By.id("password");
+//	private By loginButton    = By.name("login");
 
 	
 	public LoginPage(WebDriver driver) {
 		this.driver=driver;
+		PageFactory.initElements(driver, this);
 		
 	}
 	
 	
 	public void enterUsername(String username) {
 		
-		driver.findElement(usernameText).sendKeys(username);
+		usernameText.sendKeys(username);
+//		driver.findElement(usernameText).sendKeys(username);
 	}
 	
 	public void enterPassword(String password) {
 		
-		driver.findElement(passwordText).sendKeys(password);
+		passwordText.sendKeys(password);
+		// driver.findElement(passwordText).sendKeys(password);
 	}
 
 	public void ClickLogin() {
 	
-	driver.findElement(loginButton).click();
+		loginButton.click();
+	// driver.findElement(loginButton).click();
 }
 	
 
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,20 +1,53 @@
 package tests;
 
+import java.io.IOException;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import Base.BaseTest;
 import Pages.LoginPage;
+import Utils.ExcelUtils;
 import Utils.Log;
 
 public class LoginTest extends BaseTest {
 	
+	@DataProvider(name="LoginData")
+	public Object[][] getLoginData() throws IOException{
+		
+		String filePath = System.getProperty("user.dir") + "/testdata/TestData.xlsx";
+	    ExcelUtils.loadexcel(filePath, "sheet1");
+		int rowCount = ExcelUtils.getRowCount();
+		Object[][] data = new Object[rowCount-1][2];
+		
+		for(int i=1;i<rowCount;i++) {
+			data[i-1][0] = ExcelUtils.getcellData(i, 0);
+			data[i-1][1] = ExcelUtils.getcellData(i,1);		
+			}
+		ExcelUtils.closeExcel();
+		return data;
+	}
 	
-	@Test
-	public void testValidLogin() {
+	@DataProvider(name="LoginData2")
+	public Object[][] getData(){
+		
+		return new Object[][] {
+			{"roomtest1@simplertrading.com","4IW6i8vINHxWD@1upABBOqxG"},
+			{"roomtest2@simplertrading.com","tRqwQj&wZh%V4nN1pl2qCWz6"},
+			{"roomtest3@simplertrading.com","GrGlGx2%Xu)h69H6f%lTZy4c"},
+			{"sharathkumar.rajendran+test@c.simplertrading.com","Sharath@123"}
+		};
+	}
+	
+	@Test(dataProvider ="LoginData2")
+	public void testValidLogin(String Username, String Password) {
+		
 		        Log.info("Starting the login test");
 		        LoginPage loginPage = new LoginPage(driver);
-		        loginPage.enterUsername("roomtest1@simplertrading.com");
-		        loginPage.enterPassword("4IW6i8vINHxWD@1upABBOqxG");
+		       //  loginPage.enterUsername("roomtest1@simplertrading.com");
+		        loginPage.enterUsername(Username);
+		        // loginPage.enterPassword("4IW6i8vINHxWD@1upABBOqxG");
+		        loginPage.enterPassword(Password);
 		        loginPage.ClickLogin();
 		        System.out.println(driver.getTitle());
 		        Log.info("Verifying the page title" );
