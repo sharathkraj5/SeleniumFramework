1901d52b2092463fe1ef14922bec87cab9ac65b7
sharathkraj5 <sharathkraj5@gmail.com>
2026-05-05 16:23:50 +0530

    page factory implementation

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,15 +1,11 @@
 package Pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-	
-	private WebDriver driver;
-	
 	
 	// Page Factory method
 	@FindBy(id="username")
@@ -30,7 +26,6 @@ public class LoginPage {
 
 	
 	public LoginPage(WebDriver driver) {
-		this.driver=driver;
 		PageFactory.initElements(driver, this);
 		
 	}
